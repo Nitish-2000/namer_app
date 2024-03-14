bc21a3bf74da6df44127568eafeb175cca3c5ca9
Nitish2000 <nitishmani111@gmail.com>
2024-03-14 15:50:09 +0530

    Theme updated

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(65, 225, 225, 213)),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Center(
+      
+      child: Scaffold(
+        body: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,  // ← Add this.
+                  
+          children: [
+            Text('A Random Awesome Thoughts :'),
+            BigCard(pair: pair),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print('Button pressed by Talkies!');
+                appState.getNext();
+              },
+              child: Text('For Next Word Click Here'),
+            
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Center(
+      child: Card(
+        color : theme.colorScheme.primary,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Text(pair.asLowerCase, style: style,),
+        ),
+      ),
+    );
+  }
+}
